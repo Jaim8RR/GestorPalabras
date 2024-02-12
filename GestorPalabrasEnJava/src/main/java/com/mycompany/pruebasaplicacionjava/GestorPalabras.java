c301ec1fec4e0f14d035761f235ad0bd21c6e6ac
Jaime Riesgo Rossi <153544634+Jaim8RR@users.noreply.github.com>
2024-02-12 18:05:48 +0100

    Añado el esqueleto del proyecto
    La clase test está totalmente vacia y la clase que gestiona palabras sin revisar.

---
+++ GestorPalabrasEnJava/src/main/java/com/mycompany/pruebasaplicacionjava/GestorPalabras.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebasaplicacionjava;
+
+/**
+ *
+ * @author Jaime
+ */
+public class GestorPalabras {
+    public boolean esPalindromo(String palabra) {//si hay una tilde debe funcionar?//número?
+        String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
+        
+        for (int i = 0; i < palabraFormateada.length() / 2; i++) {
+            if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int contarVocales(String palabra) {
+        int contador = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            char caracter = Character.toLowerCase(palabra.charAt(i));
+            if (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public String invertirPalabra(String palabra) {
+        StringBuilder resultado = new StringBuilder();
+        for (int i = palabra.length() - 2; i >= 0; i--) {
+            resultado.append(palabra.charAt(i));
+        }
+        return resultado.toString();
+    }
+}
---
+++ GestorPalabrasEnJava/src/main/java/com/mycompany/pruebasaplicacionjava/PruebasAplicacionJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebasaplicacionjava;
+
+/**
+ *
+ * @author dev
+ */
+public class PruebasAplicacionJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
