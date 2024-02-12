9a19addeeabf750f3af606e02d71e0e4d668e2e8
Alex Gonzaga <alex.gonzaga@educa.madrid.org>
2024-02-12 19:25:02 +0100

    Test para esPalindromo añadido

--- GestorPalabrasEnJava/src/test/java/GestorPalabrasTest.java
+++ GestorPalabrasEnJava/src/test/java/GestorPalabrasTest.java
@@ -1,3 +1,7 @@
+
+import com.mycompany.pruebasaplicacionjava.GestorPalabras;
+import org.junit.*;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +12,9 @@
  * @author Jaime
  */
 public class GestorPalabrasTest {
-    
+    @Test
+    public void esPalindromoTest() {
+        GestorPalabras tildes = new GestorPalabras();
+        Assert.assertTrue(tildes.esPalindromo("allí, ves sevilla"));
+    }
 }
