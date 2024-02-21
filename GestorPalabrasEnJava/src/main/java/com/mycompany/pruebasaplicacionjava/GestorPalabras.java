6a83a8e915d6a5f3a560c02495e592b9f7cef1a3
jaim8RR <riesgorossijaimedaw@gmail.com>
2024-02-21 19:47:14 +0100

    cambio test isPalindrome

--- GestorPalabrasEnJava/src/main/java/com/mycompany/pruebasaplicacionjava/GestorPalabras.java
+++ GestorPalabrasEnJava/src/main/java/com/mycompany/pruebasaplicacionjava/GestorPalabras.java
@@ -4,38 +4,43 @@
  */
 package com.mycompany.pruebasaplicacionjava;
 
+import java.text.Normalizer;
+
 /**
  *
  * @author Jaime
  */
 public class GestorPalabras {
-    public boolean isPalindrome(String word) {
-        String formattedWord = word.replaceAll("\\s+", "").toLowerCase();
-        
-        for (int i = 0; i < formattedWord.length() / 2; i++) {
-            if (formattedWord.charAt(i) != formattedWord.charAt(formattedWord.length() - i - 1)) {
+
+    public boolean isPalindrome(String str) {
+        String normalizedStr = Normalizer.normalize(str, Normalizer.Form.NFD)
+                .replaceAll("\\p{M}", "");                                  //replaces white spaces with"" (i have no idea how it works but it does)
+        String cleanedStr = normalizedStr.replaceAll("[^\\p{L}\\p{N}]", "");//replaces áüî etc
+        String lowerCaseStr = cleanedStr.toLowerCase();
+        for (int i = 0; i < lowerCaseStr.length() / 2; i++) {
+            if (lowerCaseStr.charAt(i) != lowerCaseStr.charAt(lowerCaseStr.length() - i - 1)) {
                 return false;
             }
         }
-        return true;
+        return (str.length() >= 1);
     }
 
     public int countVowels(String word) {
         int counter = 0;
         for (int i = 0; i < word.length(); i++) {
-        char character = Character.toLowerCase(
-                word.charAt(i));
-        if (isVowel(character)) {
-            counter++;
+            char character = Character.toLowerCase(
+                    word.charAt(i));
+            if (isVowel(character)) {
+                counter++;
+            }
         }
-    }
         return counter;
     }
-    
+
     private boolean isVowel(char c) {
-    return "aeiouáéíóúü".indexOf(c)!= -1;
+        return "aeiouáéíóúü".indexOf(c) != -1;
     }
-    
+
     public String invertWord(String word) {
         StringBuilder result = new StringBuilder();
         for (int i = word.length() - 1; i >= 0; i--) {
